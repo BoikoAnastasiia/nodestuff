6b305d2b43b5767ed3a913ff460348cce5b171d5
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-06-11 18:32:10 +0200

    save

--- sortlongestword.js
+++ sortlongestword.js
@@ -1,4 +1,3 @@
 function sortByLength(array) {
-  console.log(array.split(', ').sort((a, b) => a.length - b.length));
-  return array.split(', ').sort((a, b) => a.length - b.length);
+  return array.sort((a, b) => a.length - b.length);
 }
