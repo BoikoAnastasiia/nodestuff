3f4a46cf73eb21a0caa97ced0167487564ae1b93
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-07-02 19:13:13 +0200

    middle

---
+++ middle.js
@@ -0,0 +1,8 @@
+function getMiddle(s) {
+  console.log(Math.ceil(s.length) / 2 - 1);
+  return s.length % 2 === 0
+    ? s[Math.ceil(s.length / 2) - 1] + s[Math.ceil(s.length) / 2]
+    : s[Math.ceil(s.length / 2) - 1];
+}
+
+console.log(getMiddle('testing'));
