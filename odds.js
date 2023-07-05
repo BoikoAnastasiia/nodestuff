ae846fb1bd633aa767b4ac983f21fabcf144b540
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-07-05 11:07:54 +0200

    save odds

---
+++ odds.js
@@ -0,0 +1,8 @@
+function findOdd(A) {
+  const set = new Set(A);
+  const vocab = Array.from(set).map((el, index) => ({ [el]: 0 }));
+
+  return obj;
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
