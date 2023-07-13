76a853b6e6a8b7b5c84b918ce37d4bf64c363871
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-07-13 22:10:42 +0200

    save

--- orig.js
+++ orig.js
@@ -4,7 +4,8 @@ const st3keyArray = [
   '0D046AF7EFCD08559C0F527CBA9E7556',
   'E81C41911C3AA55B9357A8DAC6AAF7FD',
   '894EE58DA16254EF7C1A3DF04BB049A1',
-](async () => {
+];
+(async () => {
   await Promise.all(
     st3keyArray.map((file) =>
       download(
