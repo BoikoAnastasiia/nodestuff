a5abb15ce95b6d46505579240d639fe6d42c0268
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-07-19 17:34:40 +0200

    save

---
+++ triple.js
@@ -0,0 +1,9 @@
+function tripledouble(num1, num2) {
+  let three = 1;
+  while (three < 4) {}
+  num1 + ''.split('');
+  for (let index = 0; index < num1 + ''.split('').length; index++) {
+    const element = array[index];
+  }
+}
+console.log(tripledouble(10560002, 100));
