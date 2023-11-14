fb3a2b541f19eb3d727680a1a85f2bee4837a23f
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-11-14 15:04:16 +0100

    kjbkb

--- checkForProperty.js
+++ checkForProperty.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const folderPath = './default_presets';
-// const folderPath = './test';
 const command =
   'aws s3 sync s3://gipper-static-assets/default_presets_update default_presets';
 
@@ -16,17 +15,36 @@ fs.readdir(folderPath, (err, files) => {
     const filePath = `${folderPath}/${file}`;
     try {
       const data = fs.readFileSync(filePath, 'utf8');
+
+      // const testJSON = (string) => {
+      //   if (typeof string === 'string') {
+      //     return false;
+      //   }
+      //   try {
+      //     JSON.parse(data);
+      //     return true;
+      //   } catch (error) {
+      //     return false;
+      //   }
+      // };
+      // console.log(testJSON(data));
+
+      // const jsonString = JSON.stringify(json);
+      // console.log(jsonString);
+      // if (jsonString.includes('"clipPath":{"type":"image"')) {
+      //   newArr.push(file);
+      //   return;
+      // }
+
       const json = JSON.parse(data);
-      const jsonString = JSON.stringify(json);
-      console.log(jsonString);
-      if (jsonString.includes('"clipPath":{"type":"image"')) {
-        newArr.push(file);
-        return;
+
+      if (json?.body?.objects) {
+        json.body.objects.filter((obj) => obj.type != 'image' && obj.clipPath)
+          .length > 0 && newArr.push(file);
       }
     } catch (parseError) {
       console.error(`Error parsing JSON in file ${file}:`, parseError);
     }
   });
   fs.writeFileSync('imageclippath.txt', newArr.join('\n'));
-  // console.log('Results written to propertyList.txt');
 });
