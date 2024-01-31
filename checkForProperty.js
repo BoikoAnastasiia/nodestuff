5360f769ad0309c58e668ab53e66b7f2adb3f6a1
Nastya Boiko <boiko.nastasiia@gmail.com>
2024-01-31 19:37:28 +0100

    save

--- checkForProperty.js
+++ checkForProperty.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
 const folderPath = './default_presets';
-// const folderPath = './animated_templates';
-const command =
-  'aws s3 sync s3://gipper-static-assets/default_presets_update default_presets';
-
-const command2 =
-  'aws s3 sync s3://gipper-static-assets/animated_templates animated_templates';
 
 fs.readdir(folderPath, (err, files) => {
   if (err) {
@@ -18,26 +12,17 @@ fs.readdir(folderPath, (err, files) => {
   files.forEach((file) => {
     const filePath = `${folderPath}/${file}`;
     try {
-      const data = fs.readFileSync(filePath, 'utf8');
+      const jsonString = fs.readFileSync(filePath, 'utf8');
+      const json = JSON.parse(jsonString);
 
-      // console.log(testJSON(data));
-
-      const jsonString = JSON.stringify(data);
-      // console.log(jsonString);
-      if (jsonString.includes('"strokeWidth": 7')) {
-        newArr.push(file);
-        return;
+      if (json?.body?.objects) {
+        json.body.objects.filter((obj) => obj.backgroundColor !== null).length >
+          0 && newArr.push(file);
       }
-
-      // const json = JSON.parse(data);
-
-      // if (json?.body?.objects) {
-      //   json.body.objects.filter((obj) => obj.type != 'image' && obj.clipPath)
-      //     .length > 0 && newArr.push(file);
-      // }
     } catch (parseError) {
       console.error(`Error parsing JSON in file ${file}:`, parseError);
     }
   });
-  fs.writeFileSync('circle.txt', newArr.join('\n'));
+
+  fs.writeFileSync('background.txt', newArr.join('\n'));
 });
