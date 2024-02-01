5a712540c1b475eaca45fe3c0f6b2ac69b903810
Nastya Boiko <boiko.nastasiia@gmail.com>
2024-02-01 15:20:26 +0100

    save

--- checkForProperty.js
+++ checkForProperty.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const folderPath = './default_presets';
+const command =
+  'aws s3 sync s3://gipper-static-assets/default_presets_update default_presets';
 
 fs.readdir(folderPath, (err, files) => {
   if (err) {
@@ -14,19 +16,24 @@ fs.readdir(folderPath, (err, files) => {
     try {
       const jsonString = fs.readFileSync(filePath, 'utf8');
 
-      if (jsonString.includes('chalk') && jsonString.includes('gradient')) {
-        newArr.push(file);
-      }
       const json = JSON.parse(jsonString);
 
-      // if (json?.body?.objects) {
-      //   json.body.objects.filter((obj) => obj.backgroundColor !== null).length >
-      //     0 && newArr.push(file);
-      // }
+      if (json?.body?.objects) {
+        json?.body?.objects.forEach((obj) => {
+          if (obj.fill.colorStops[0].color !== obj.fill.colorStops[1].color) {
+            newArr.push(file);
+          }
+          if (obj.objects) {
+            obj.objects.some((el) => {
+              el.fill.colorStops[0].color !== el.fill.colorStops[1].color;
+            }) && newArr.push(file);
+          }
+        });
+      }
     } catch (parseError) {
       console.error(`Error parsing JSON in file ${file}:`, parseError);
     }
   });
 
-  fs.writeFileSync('background.txt', newArr.join('\n'));
+  fs.writeFileSync('gradientsCrooked.txt', newArr.join('\n'));
 });
