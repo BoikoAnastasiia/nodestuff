2431fa83bb61c8fa38ca6ec9b53cd323c92fb481
Nastya Boiko <boiko.nastasiia@gmail.com>
2024-03-26 12:43:45 +0100

    searching for svg

--- checkForProperty.js
+++ checkForProperty.js
@@ -16,40 +16,18 @@ fs.readdir(folderPath, (err, files) => {
 
       const json = JSON.parse(jsonString);
 
-      if (json?.body?.objects) {
-        json.body.objects.forEach((obj) => {
-          try {
-            if (obj.fill.colorStops[0].color !== obj.fill.colorStops[1].color) {
-              newArr.push(file);
-            }
-            if (obj.objects) {
-              const hasDifferentGradient = obj.objects.some((el) => {
-                try {
-                  return (
-                    el.fill.colorStops[0].color !== el.fill.colorStops[1].color
-                  );
-                } catch (innerError) {
-                  console.error(
-                    `Error parsing JSON in nested object of file ${file}:`,
-                    innerError
-                  );
-                  return false; // Continue with other objects in the array
-                }
-              });
-
-              if (hasDifferentGradient) {
-                newArr.push(file);
-              }
-            }
-          } catch (innerError) {
-            console.error(`Error parsing JSON in file ${file}:`, innerError);
-          }
-        });
+      if (
+        jsonString.includes('.svg') &&
+        !jsonString.includes(
+          'https://gipper-static-assets.s3.amazonaws.com/icons/move_cursor.svg'
+        )
+      ) {
+        newArr.push(file);
       }
     } catch (parseError) {
       console.error(`Error parsing JSON in file ${file}:`, parseError);
     }
   });
 
-  fs.writeFileSync('gradientsCrooked.txt', newArr.join('\n'));
+  fs.writeFileSync('svg.txt', newArr.join('\n'));
 });
