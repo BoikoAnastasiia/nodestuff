5ac28455a83ce8feba52aa8ca3691f8c726fcf9c
Nastya Boiko <boiko.nastasiia@gmail.com>
2024-04-25 14:07:10 +0200

    3971

--- locationSort.js
+++ locationSort.js
@@ -10,7 +10,7 @@ fs.readdir(folderPath, (err, files) => {
 
   const Locations = [];
   const baseUrl = 'https://platform.gogipper.com/templates/';
-  const templateId = '3962';
+  const templateId = '3971';
 
   files.forEach((file) => {
     const filePath = `${folderPath}/${file}`;
